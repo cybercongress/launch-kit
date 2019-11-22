051318bf12d6179be70a321c8fd227fd6c691e3d
Ales Puchilo <ales.puchilo@gmail.com>
2019-11-22 19:13:57 +0300

    Add gen_txs_collector script

---
+++ gen_txs/gen_txs_collector.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+print("loading json...")
+
+with open('data/genesis.json', 'r') as f:
+    genesis_dict = json.load(f)
+
+print("genesis.json loaded")
+
+path = "data/gen_txs/"
+gentxslist = os.listdir(path)
+
+i = 0
+for file in gentxslist:
+    gentxslist[i] = path + gentxslist[i]
+    i+=1
+
+gentxs = []
+
+for file in gentxslist:
+    with open(file, 'r') as f:
+        gentxs.append(json.load(f))
+
+print("gen_tx files loaded")
+
+genesis_dict["app_state"]["gentxs"] = gentxs
+
+print("gentxs added")
+
+print("saving to signed_genesis.json")
+
+with open('data/signed_genesis.json', 'w') as fp:
+    json.dump(genesis_dict, fp)
+
+print("done")
